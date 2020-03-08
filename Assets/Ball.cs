1817454638a5f23f06402f77f5615b92f5857845
junhyungjin <junhyungjin@gmail.com>
2020-03-09 01:33:55 +0900

    third commit

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ParticleSystem explosionParticle;
+    public AudioSource explosionAudio;
+    public float maxDamage = 100f;
+    public float explosionForce = 1000f;
+    public float lifeTime = 10f;
+    public float explosionRadius = 20f;
+
     void Start()
     {
-        
+        Destroy( gameObject, lifeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        explosionParticle.transform.parent = null;
+
+        explosionParticle.Play();
+        explosionAudio.Play();
+
+        Destroy(explosionParticle.gameObject, explosionParticle.duration);
+        Destroy(gameObject);
     }
 }
